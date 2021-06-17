325b581c3be56015db173a51637ae2cab5935f86
Vadym Shakhraichuk <shakhraichuk@gmail.com>
2021-06-17 00:52:31 -0700

    Express Router

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const dishRouter = require('./routes/dishRouter');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+app.use(bodyParser.json());
 
+app.use('/dishes', dishRouter);
 app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
