79b78045c7ed186185790bdb7a7c59bc6cbade0e
Vadym Shakhraichuk <shakhraichuk@gmail.com>
2021-06-30 00:37:47 -0700

    Assignment 1: Node Modules, Express and REST API

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -12,7 +14,12 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
+
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
+
+
 app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
